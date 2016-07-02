5d117ac595190f706bbb9f85f9a399379fbc2a81
eduaguilar <eduaguilar1@hotmail.com>
2016-07-02 19:56:10 +0000

    Ejercicio 1 del Examen Resuelto Puntos a),b) y c)Implementacion de Funciones par la carga de libros y para listar libros.

--- Examenes/Examen2/SegundoExamen.c
+++ Examenes/Examen2/SegundoExamen.c
@@ -31,10 +31,15 @@ struct Libro {
 };
 
 //Declaro la estructura que voy a utilizar Libros de tipo Libro
-struct Libro Libros;
+struct Libro Libros[50];
+
+//Declaracion de Funciones
+void IngresoDeLibros(int cant);
+void ListarLibros(int i);
 
 //Declaración de variables
 char 	resp;
+int		i,cant=0;
 
 
 int main(){
@@ -51,12 +56,23 @@ int main(){
 		
 		case '1':
 		system("clear");
-		printf("\n***Aquí irá el codigo para ingresar libros***\n");
+		
+		do{
+			system("clear");
+			printf("\n¿Cuantos libros desea agregar?\nPor favor ingrese cantidad: ");
+			scanf("%d",&cant);
+		}while(!cant,getchar()!='\n');
+		
+		IngresoDeLibros(cant);
 		break;
 	
 		case '2':
 		system("clear");
-		printf("\n***Aquí ira el codigo para listar libros***\n");
+						
+		printf("Sus Libros cargados son los siguientes: \n");
+			for(i=0;i<cant;i++){
+				ListarLibros(i);
+			}
   		break;
 
 		default:break;
@@ -65,3 +81,36 @@ int main(){
 	system("clear");
 	printf("\n***Hasta pronto, Muchas gracias***\n");
 }	
+
+
+//Funciones
+
+void IngresoDeLibros (int cant){//Para cargar libros nuevos
+
+	printf("Por favor cargue sus Libros:... \n");
+	
+	for(i=0;i<cant;i++){
+		do{
+			printf("\nIngrese ID del Libro: ");
+			scanf("%d",Libros[i].ID);
+		}while(!(Libros[i].ID),getchar()!='\n');
+
+		printf("\nIngrese Nombre: ");
+		fgets(Libros[i].nombre, 50, stdin);
+		printf("\nIngrese Autor: ");
+		fgets(Libros[i].autor, 100, stdin);
+		printf("\nIngrese Categoria: ");
+		fgets(Libros[i].categoria, 50, stdin);
+		
+		system("clear");
+	}
+}
+
+void ListarLibros(int i){//Para Imprimir por pantalla
+
+	printf("****************************\n");
+	printf( "ID Libro: %d", *Libros[i].ID);
+	printf( "\nNombre: %s", Libros[i].nombre);
+	printf( "Autor: %s", Libros[i].autor);
+	printf( "Categoria: %s", Libros[i].categoria);
+}
