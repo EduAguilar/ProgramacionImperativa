f6dca7b86b0cd2d0274a33b96808bb7e5a515c55
eduaguilar <eduaguilar1@hotmail.com>
2016-07-04 21:01:26 +0000

    Ejercicio 3 del Examen, Resuelto hasta Editar Registros, Funcionando correctamente.

--- Examenes/Examen2/SegundoExamen.c
+++ Examenes/Examen2/SegundoExamen.c
@@ -20,27 +20,35 @@ b- Mostrar Menú de opciones
 c- Implementar funciones para la la salida en pantalla
 
 2) Usar archivo de texto como base de datos.
+
+3)Agregar las opciones
+
+	3-Editar Registros(Libro)
+	4-Eliminar Registros
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 //Defino una estructura tipo Libro con sus determinados campos de variables
 struct Libro {
-   int     ID[50];
+   char    ID[50];
    char    nombre[50]; 
    char    autor[100];
    char    categoria[50];
 };
 
 //Declaro la estructura que voy a utilizar Libros de tipo Libro
-struct Libro Libros[50];
+struct Libro Libros;
 
 //Declaracion de Funciones
-void IngresoDeLibros(int cant);
-void ListarLibros(int i);
-void GuardarEnBD(int cant);
+void IngresoDeLibros();
+void PrintLibros();
+void ListarLibros();
+void GuardarEnBD();
+void EditarRegistro();
 
 //Declaración de variables
 char 	resp,resp2;
@@ -52,9 +60,10 @@ int main(){
 
 	do{
 		do{
+			system("clear");
 			printf("\n\t\tMenú - Sistema de Libros\n");
 			printf("\t*****************************************\n");
-			printf("\nElija entre las siguientes opciones\n\n1)- Ingresar Nuevo Libro (1). \n2)- Listar Libro (2).\nX)- Salir (X).\n");
+			printf("\nElija entre las siguientes opciones\n\n1)- Ingresar Nuevo Libro (1). \n2)- Listar Libro (2).\n3)- Editar registros (3). \n4)- Eliminar registros (4). \nX)- Salir (X).\n");
 			scanf("%c",&resp);
 		}while(!resp,getchar()!='\n');
 
@@ -62,40 +71,34 @@ int main(){
 		
 		case '1':
 		system("clear");
-		
 		do{
-			system("clear");
-			printf("\n¿Cuantos libros desea agregar?\nPor favor ingrese cantidad: ");
-			scanf("%d",&cant);
-		}while(!cant,getchar()!='\n');
-		
-		do{
-			system("clear");
-			IngresoDeLibros(cant);
-			do{
-				printf("\nUsted ha ingresado los siguientes Libros: \n");
-				for(i=0;i<cant;i++){
-					ListarLibros(i);
-				};
-			printf("\n\nCompruebe si sus datos son correctos. \n - Presione (S) si desea guardar los datos o (N) si quiere corregir los datos antes de guardarlos\n");
-			scanf("%c",&resp2);
+			IngresoDeLibros();
+			printf("\nUsted ha ingresado el siguiente Libro: \n");
+			PrintLibros();
+
+			do{	
+				printf("\n\nCompruebe si sus datos son correctos. \n - Presione (S) si desea guardar los datos o (N) si quiere corregir los datos antes de guardarlos\n");
+				scanf("%c",&resp2);
 			}while(!resp2,getchar()!='\n');
 		}while(resp2!='s'&& resp2!='S');
-		
-		archivo=fopen("BD_Libros.txt","a+r+");
-		GuardarEnBD(cant);
-		fclose(archivo);
-		
-		
+		GuardarEnBD();
 		break;
 	
 		case '2':
 		system("clear");
-						
 		printf("Sus Libros cargados son los siguientes: \n");
-			for(i=0;i<cant;i++){
-				ListarLibros(i);
-			}
+		ListarLibros();
+		break;
+  		
+  		case '3':
+  		system("clear");
+  		EditarRegistro();
+  		
+  		break;
+  		
+  		case '4':
+  		system("clear");
+  		
   		break;
 
 		default:break;
@@ -108,42 +111,196 @@ int main(){
 
 //Funciones
 
-void IngresoDeLibros (int cant){//Para cargar libros nuevos
+void IngresoDeLibros (){//Para cargar libros nuevos
 
-	printf("\nPor favor cargue sus Libros:... \n");
-	
-	for(i=0;i<cant;i++){
-		do{
-			printf("\nIngrese ID del Libro: ");
-			scanf("%d",Libros[i].ID);
-		}while(!(Libros[i].ID),getchar()!='\n');
+	printf("\nPor favor cargue los datos de su Libro:... \n");
 
-		printf("\nIngrese Nombre: ");
-		fgets(Libros[i].nombre, 50, stdin);
-		printf("\nIngrese Autor: ");
-		fgets(Libros[i].autor, 100, stdin);
-		printf("\nIngrese Categoria: ");
-		fgets(Libros[i].categoria, 50, stdin);
-		
-		system("clear");
-	}
+	printf("\nIngrese ID del Libro: ");
+	fgets(Libros.ID, 50, stdin);
+	printf("\nIngrese Nombre: ");
+	fgets(Libros.nombre, 50, stdin);
+	printf("\nIngrese Autor: ");
+	fgets(Libros.autor, 50, stdin);
+	printf("\nIngrese Categoria: ");
+	fgets(Libros.categoria, 50, stdin);
 }
 
-void ListarLibros(int i){//Para Imprimir por pantalla
-
+void PrintLibros(){//Para Imprimir libros por pantalla
 	printf("****************************\n");
-	printf( "ID Libro: %d", *Libros[i].ID);
-	printf( "\nNombre: %s", Libros[i].nombre);
-	printf( "Autor: %s", Libros[i].autor);
-	printf( "Categoria: %s", Libros[i].categoria);
+	printf( "ID Libro: %s", Libros.ID);
+	printf( "Nombre: %s", Libros.nombre);
+	printf( "Autor: %s", Libros.autor);
+	printf( "Categoria: %s", Libros.categoria);
 }
 
-void GuardarEnBD(int cant){
-	for(i=0;i<cant;i++){
-		fprintf( archivo,"****************************\n");
-		fprintf( archivo,"ID Libro: %d\n", *Libros[i].ID);
-		fprintf( archivo,"\nNombre: %s\n", Libros[i].nombre);
-		fprintf( archivo,"Autor: %s\n", Libros[i].autor);
-		fprintf( archivo,"Categoria: %s\n", Libros[i].categoria);
-	}
+void ListarLibros(){//Para Listar libros desde la Base de Datos
+	archivo=fopen("BD_Libros.txt","r+b");
+	
+	while(!feof (archivo)){
+		
+		fread(&Libros,sizeof(Libros),1,archivo);//Para leer la estructura desde la base de datos
+		
+		if(!feof(archivo)){
+		printf("****************************\n");
+		printf( "ID Libro: %s", Libros.ID);
+		printf( "Nombre: %s", Libros.nombre);
+		printf( "Autor: %s", Libros.autor);
+		printf( "Categoria: %s", Libros.categoria);
+		};
+	};
+	getchar();
+	fclose(archivo);
 }
+
+void GuardarEnBD(){//Para guardar en la Base de datos
+	archivo=fopen("BD_Libros.txt","a+r+");
+	fwrite(&Libros,sizeof(Libros),1,archivo);
+	fclose(archivo);
+}
+
+void EditarRegistro(){
+	char id[10];
+	char resp3;
+	int tama;
+	system("clear");
+	tama=sizeof(Libros);//verifica el tamaño de la estructura y se la asigna a la variable
+	archivo = fopen("BD_Libros.txt", "rb+");
+		printf("Ingrese el id del libro a editar: ");
+		fgets(id,10,stdin);// se carga el conjunto de datos a buscar en la estructura, pueden ser varios 
+		//getchar();
+		system("clear");
+	
+		while(!feof(archivo)){
+			fread(&Libros,sizeof(Libros),1,archivo);// lee la estructura del archivo 
+			 if (strcmp(id,Libros.ID)==0){
+			
+			printf("\nEl registro que usted desea modificar es el siguiente:\n");
+			printf("\n*******************************\n");
+			printf("1)- ID: %s",Libros.ID);
+			printf("2)- NOMBRE: %s",Libros.nombre);
+			printf("3)- AUTOR: %s",Libros.autor);
+			printf("4)- CATEGORIA: %s",Libros.categoria);
+			printf("S)- S (MINUSCULA) Para salir de la edición \n");
+			printf("*******************************\n");
+		
+			
+				while(resp3!='s'){
+					printf("Por favor seleccione la opcion de la cual desea modificar su valor:");
+					resp3=getchar();
+					//getchar();
+					//resp3=toupper(getchar());
+					getchar();
+					
+					
+					switch (resp3){
+						case '1':
+						printf("Ingrese el nuevo ID: ");
+						fgets(Libros.ID,10,stdin);
+						break;
+						case '2':
+						printf("Ingrese el nuevo NOMBRE: ");
+						fgets(Libros.nombre,50,stdin);
+						break;
+						case '3':
+						printf("Ingrese el nuevo AUTOR: ");
+						fgets(Libros.autor,50,stdin);
+						break;
+						
+						case '4':
+						printf("Ingrese la nueva CATEGORIA: ");
+						fgets(Libros.categoria,50,stdin);
+						break;
+						
+						case 's':
+						fseek(archivo,-tama, SEEK_CUR);
+						fwrite(&Libros,sizeof(Libros),1,archivo);
+						system("clear");
+						break;
+						
+						default:
+						break;
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		fclose(archivo);
+	}
+
+/*
+void EditarRegistro(){
+	
+	char ValorBuscado[50];//Variable donde se almacenara temporalmente el valor buscado a editar
+	char resp3;
+	int tam;//Variable que se utilizará para guardar el tamaño de la estructura
+	
+	tam=sizeof(Libros);//Se asigna el tamaño de la estructura a la variable
+	
+	archivo = fopen("BD_Libros.txt","a+rb+");
+	printf("Usted procederá a modificar el registro de un libro...\n");
+	printf("\nPor favor ingrese el ID del libro a editar: ");
+	fgets(ValorBuscado,50,stdin);
+	
+	system("clear");
+	
+	while(!feof(archivo)){
+		fread(&Libros,sizeof(Libros),1,archivo);// lee la estructura del archivo
+		
+		if (strcmp(ValorBuscado,Libros.ID)==0){//Se compara la Variable a buscar con los nombres de los libros 
+											   //almacenados en la Base de Datos, si este coincide entonces procede y muestra un menú
+			printf("\nEl registro que usted desea modificar es el siguiente:\n");
+			printf("\n*******************************\n");
+			printf("1)- ID: %s",Libros.ID);
+			printf("2)- NOMBRE: %s",Libros.nombre);
+			printf("3)- AUTOR: %s",Libros.autor);
+			printf("4)- CATEGORIA: %s",Libros.categoria);
+			printf("X)- X Para salir de la edición \n");
+			printf("*******************************\n");
+		
+			while(resp3!='X'&&resp3!='x'){
+			printf("\nPor favor seleccione la opcion de la cual desea modificar su valor:");
+			scanf("%s",&resp3);
+			getchar();
+			
+			switch (resp3){
+				case '1':
+				printf("\nIngrese el nuevo ID: ");
+				fgets(Libros.ID,50, stdin);
+				break;
+				
+				case '2':
+				printf("Ingrese el nuevo NOMBRE: ");
+				fgets(Libros.nombre,50,stdin);
+				break;
+				
+				case '3':
+				printf("Ingrese el nuevo AUTOR: ");
+				fgets(Libros.autor,50,stdin);
+				break;
+				
+				case '4':
+				printf("Ingrese la nueva CATEGORIA: ");
+				fgets(Libros.categoria,50,stdin);
+				break;
+				
+				case 'X':
+				fseek(archivo,-tam, SEEK_CUR);
+				fwrite(&Libros,sizeof(Libros),1,archivo);
+				system("clear");
+				break;
+				default:
+				break;
+				}
+			}
+	
+		}
+		
+		//if (strcmp(ValorBuscado,Libros.nombre)==1){
+		//	printf("\nEl libro que usted busca editar no se encuentra registrado en la base de datos, o ha ingresado un nombre incorrecto\n");
+		//}
+	}
+	fclose(archivo);
+}*/
